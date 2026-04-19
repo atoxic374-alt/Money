2e4e12a2be13fa75a95fdaeff8720c270cb42c5a
atoxic374-alt <264734154+atoxic374-alt@users.noreply.github.com>
2026-04-19 17:45:42 -0400

    Extract music-updated.rar

---
+++ music-updated/music-custom (1)/handler/reasdy.js
@@ -0,0 +1,30 @@
+const { TwitchUrl, statuses } = require(`${process.cwd()}/settings/config`);
+
+module.exports = {
+  name: 'ready',
+    // discord.js event handler option: emit once
+  once: true,
+  async execute(client) {
+
+    console.table({
+      Name: client.user.tag,
+      BotId: client.user.id,
+      Server: client.guilds.cache.size,
+      Members: client.users.cache.size,
+      Channels: client.channels.cache.size,
+    });
+
+
+    client.user.setPresence({
+      status: 'dnd',
+      activities: [
+        {
+          name: `${statuses}`,
+          type: 1,
+          url: `${TwitchUrl}`
+        }
+      ]
+    });
+
+  },
+};
